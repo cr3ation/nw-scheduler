27cc9701574d1cd1cffc9f2e91c9f939bb89eed4
Henrik Engström <me@henrikengstrom.com>
2025-02-03 12:22:29 +0100

    Added support for static files
    This will ensure that the admin page looks and behaves as expected.

--- django/app/app/settings.py
+++ django/app/app/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,8 +127,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# The absolute path to the directory where collectstatic will collect static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Trusted origins, such as 'https://subdomain.example.com'
 CSRF_TRUSTED_ORIGINS = [
