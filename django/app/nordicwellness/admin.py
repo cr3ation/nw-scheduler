f7476bc554f9d3043cd73d0f573b4291370bdc25
Henrik Engström <me@henrikengstrom.com>
2025-02-20 23:58:36 +0100

    Admin site header updated

--- django/app/nordicwellness/admin.py
+++ django/app/nordicwellness/admin.py
@@ -7,4 +7,7 @@ class ActivityAdmin(admin.ModelAdmin):
     ordering = ('-starttime',)
 
 
+admin.site.site_header = "Nordic Wellness Scheduler Admin"
+admin.site.site_title = "Nordic Wellness Scheduler Admin Portal"
+admin.site.index_title = "Welcome to Nordic Wellness Scheduler Admin"
 admin.site.register(Activity, ActivityAdmin)
